b25aa4d8619e140a3878d91658736d32f9c0aab7
anabelcastillo <anabelcas@gmail.com>
2023-03-14 22:22:03 +0100

    "Exercici 2"

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Story } from '../story';
+
+// export interface Story {
+//   text: string;
+// }
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
 
+export class HomeComponent implements OnInit {
+
+  stories: Story[] =  [
+    {text: "El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial"},
+    {text: "Sentia curiositat per l'interior de la nau i es va posar a inspeccionar-la. Va arribar a una sala amb dues portes"},
+    {text: "L'heroi va decidir travessar la porta que el portava a casa"},
+    {text: "Mentrestant, altres heroes no van tenir tanta sort en la seva elecció ..."}
+  ];
+
+  constructor() { };
+
+  ngOnInit(): void {
+
+  };
 }
--- src/app/scene/scene.component.ts
+++ src/app/scene/scene.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Story } from '../story';
 
 @Component({
   selector: 'app-scene',
   templateUrl: './scene.component.html',
   styleUrls: ['./scene.component.css']
 })
-export class SceneComponent {
+
+export class SceneComponent implements OnInit {
+
+  @Input() storyList: Story = {text:''};
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 
 }
